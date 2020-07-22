034764005b67149c37e95ec36d7771cc52301cc3
MalteSli <uni@picsadd.com>
2020-07-22 16:44:27 +0200

    HelloDatabase.py und README angepasst

--- HelloDatabase.py
+++ HelloDatabase.py
@@ -2,7 +2,7 @@ import psycopg2
 
 def go():
     conn = None    
-    conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="20postgres20")
+    conn = psycopg2.connect(host="localhost", database="dbs_project_covid19", user="postgres", password="20postgres20")
     cur = conn.cursor()
     cur.execute('SELECT version();')
     for record in cur:
@@ -10,7 +10,6 @@ def go():
     cur.close()            
     if conn is not None:
         conn.close()
-        print('Database connection closed.')
 try:
     go()
 except (Exception, psycopg2.DatabaseError) as error:
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # DBS-Projekt
-Ein Projekt des Moduls "Datenbanksysteme SS20" über die Derwaltung und Darstellung von Covid-19 Daten.
+Ein Projekt des Moduls "Datenbanksysteme SS20" über die Derwaltung und Darstellung von Covid-19 Daten
 
+Ich habe in pgAdmin4 eine neue Datenbank namens "dbs_project_covid19" erstellt, in der ich die Daten speichern werde.
+Diese Datebank nutzt jetzt natürlich PostgreSQL.
+Am besten macht ihr es genauso, damit wir nicht ständig den Code anpassen müssen.
+Passwort und Nutzname müsst ihr auf euch anpassen.
