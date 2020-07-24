4e4920a24e08a665f96359a472e73ebf78689b0a
MalteSli <uni@picsadd.com>
2020-07-24 16:41:23 +0200

    processCovid19json.py geaendert

--- processCovid19json.py
+++ processCovid19json.py
@@ -1,29 +1,29 @@
 import psycopg2
 
-create_table_country = """CREATE TABLE Country (
+create_table_country = '''CREATE TABLE Country (
     c_name varchar,
     c_country_code varchar,
     "c_geoID" varchar,
     c_continent varchar,
     c_population_density varchar,
     c_hospital_beds_pt integer,
-    primary key ("c_geoID"));"""
+    primary key ("c_geoID"));'''
 
-create_table_daydata = """CREATE TABLE IF NOT EXISTS DayData (
+create_table_daydata = '''CREATE TABLE IF NOT EXISTS DayData (
     d_dayID SERIAL PRIMARY KEY,
     d_population integer,
     d_cases integer,
     d_deaths integer,
     d_test integer,
-    d_sringency_index decimal);"""
+    d_sringency_index decimal);'''
 
-create_table_has = """CREATE TABLE IF NOT EXISTS Has (
+create_table_has = '''CREATE TABLE IF NOT EXISTS Has (
     h_ID SERIAL PRIMARY KEY,
     h_geoID integer,
     h_dayID integer,
     h_day integer,
     h_month integer,
-    h_year integer);"""
+    h_year integer);'''
 
 try:
     conn = None    
@@ -31,8 +31,11 @@ try:
     cur = conn.cursor()
     print ( conn.get_dsn_parameters(),"\n")
     cur.execute(create_table_country)
+    conn.commit()
     cur.execute(create_table_daydata)
+    conn.commit()
     cur.execute(create_table_has)
+    conn.commit()
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
 finally:
